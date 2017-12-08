09726a2919f0512420e2bd206d9a47b1b772f518
msuhovec <suhovecky@nd.edu>
2017-12-08 14:35:25 -0500

    adding reds.yml config symlink (dltp-1172)
    #1

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -21,6 +21,6 @@ set :domain,    fetch(:host, 'testlibnd-curax-dev.library.nd.edu')
 set :bundle_without, %w{development test doc}.join(' ')
 set :shared_directories,  %w(log)
 set :shared_files, %w()
-set :linked_files, ['config/database.yml']
+set :linked_files, ['config/database.yml', 'config/redis.yml']
 server fetch(:domain), user: fetch(:user), roles: %w{web app db}
 
