0dfe564445475b6d7733947175739792d3021d91
msuhovec <suhovecky@nd.edu>
2017-12-13 15:26:33 -0500

    add symlinks for additional needed secrets (dltp-1172)
    #1

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -21,6 +21,6 @@ set :domain,    fetch(:host, 'testlibnd-curax-dev.library.nd.edu')
 set :bundle_without, %w{development test doc}.join(' ')
 set :shared_directories,  %w(log)
 set :shared_files, %w()
-set :linked_files, ['config/database.yml', 'config/redis.yml']
+set :linked_files, ['config/blacklight.yml', 'config/database.yml', 'config/fedora.yml', 'config/role_map.yml', 'config/redis.yml', 'config/secrets.yml', 'config/solr.yml']
 server fetch(:domain), user: fetch(:user), roles: %w{web app db}
 
