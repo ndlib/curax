95ea6e9ec3134e1f49c9b2257110486fa29270db
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2018-01-10 11:25:32 -0500

    Replacing database authentication with CAS auth
    Following https://github.com/nbudin/devise_cas_authenticatable README
    on swapping database authentication for CAS authentication. This commit
    does not include the configuration information.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,16 @@ class User < ApplicationRecord
   include Hyrax::User
   include Hyrax::UserUsageStats
 
-
-
   if Blacklight::Utils.needs_attr_accessible?
     attr_accessible :email, :password, :password_confirmation
   end
   # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  # cas_authenticatable: CAS authentication support for Devise (https://github.com/nbudin/devise_cas_authenticatable)
+  # rememberable: manages generating and clearing a token for remembering the user from a saved cookie. (http://www.rubydoc.info/github/plataformatec/devise/master/Devise/Models/Rememberable)
+  # trackable: tracks sign in count, timestamps and IP address. (http://www.rubydoc.info/github/plataformatec/devise/master/Devise/Models/Trackable)
+  devise :cas_authenticatable, :rememberable, :trackable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
