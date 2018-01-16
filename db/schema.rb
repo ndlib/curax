63a79d1ccead585a410507a92597bd3817c8824d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2018-01-16 09:19:45 -0500

    Migrating database
    ```console
    $ bundle exec rake db:migrate
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110162655) do
+ActiveRecord::Schema.define(version: 20180116141522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -490,8 +490,7 @@ ActiveRecord::Schema.define(version: 20180110162655) do
     t.string "zotero_userid"
     t.string "preferred_locale"
     t.string "username", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["username"], name: "index_users_on_username"
   end
 
   create_table "version_committers", id: :serial, force: :cascade do |t|
